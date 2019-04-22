5c8af389d832635b4f8cc3b031d22aff8dcda943
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-22 10:25:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,5 +19,10 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
+  
+  get "/multiply" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
 
 end
