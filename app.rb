deaf471bf2d52d84fe9963622917bbbf35572261
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-22 10:25:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,9 +20,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-  get "/multiply" do
-    @user_name = params[:name]
-    "Hello #{@user_name}!"
-  end
+  get "/multiply/:num1/:num2" do
+  	@product = params[:num1].to_i * params[:num2].to_i
+  	"#{@product}"
 
 end
